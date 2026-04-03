6d2c30a911f98a098eaab68bc9751326c463608b
Life Light <lifelightdev@gmail.com>
2026-04-03 12:02:31 +0100

    Format

--- src/main/java/life/light/BackgroundEvent.java
+++ src/main/java/life/light/BackgroundEvent.java
@@ -11,16 +11,12 @@ import java.lang.System.Logger;
 public class BackgroundEvent extends PdfPageEventHelper {
 
     private static final Logger logger = System.getLogger(BackgroundEvent.class.getName());
-
     private final Image img;
 
     public BackgroundEvent(String imagePath) throws Exception {
         this.img = Image.getInstance(imagePath);
         // Positionne l'image en bas à gauche (0,0)
         this.img.setAbsolutePosition(0, 0);
-        // Ajuste l'image à la taille de la page (ex: A4)
-        // Les dimensions A4 sont environ 595x842 points
-        this.img.scaleAbsolute(595, 842);
     }
 
     @Override
